f97d4e4ae19883f5d96951feb00371f52e286d74
LamberKeep <LamberKeep@gmail.com>
2022-07-10 00:11:43 +0300

    token fix

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
+import os
+
 # If you don’t know where to get token, follow this tutorial: https://core.telegram.org/bots#3-how-do-i-create-a-bot
-token = "YOUR_TOKEN_HERE"
+token = os.environ["ACCESS_TOKEN"] # run in heroku
+# token = "YOUR_TOKEN_HERE" # run in local
 
 # Debug levels: "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL".
 debug = "WARNING"
