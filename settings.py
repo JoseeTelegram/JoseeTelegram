f16bc99e06ecd7c20f3454b413a79385f7c5e325
LamberKeep <LamberKeep@gmail.com>
2022-08-11 19:27:28 +0300

    settings update

--- settings.py
+++ settings.py
@@ -1,17 +1,17 @@
 import os
 
 # If you don’t know where to get token, follow this tutorial: https://core.telegram.org/bots#3-how-do-i-create-a-bot
-token = os.environ["ACCESS_TOKEN"] # for heroku
-# token = "ACCESS_TOKEN" # for local
+token = os.environ["JOSEE_ACCESS_TOKEN"] # heroku
+# token = "ACCESS_TOKEN" # local
 
 # Your ID is required to receive error messages directly in PM.
 # It can be obtained by writing the "/start" to the bot: https://t.me/userinfobot.
 # Leave the value at 0 to disable this feature. (But why?)
-id = os.environ["MY_ID"] # for heroku
-# id = 0 # for local
+id = os.environ["JOSEE_MY_ID"] # heroku
+# id = 0 # local
 
 # Checks the current version of the bot with the version on github.
-check_version = True
+check_version = False
 
 # Checks data files and downloads them if they are not found.
 check_data = False
