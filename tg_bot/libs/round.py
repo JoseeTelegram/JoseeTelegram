adf8f703be8e86cc2d92c484bb385190fa9e9e64
LamberKeep <LamberKeep@gmail.com>
2022-09-06 07:55:57 +0300

    deleting libs

--- tg_bot/libs/round.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def RoundTo(num, digits=2):
-  if num == 0: return 0
-  scale = int(-math.floor(math.log10(abs(num - int(num))))) + digits - 1
-  if scale < digits: scale = digits
-  return round(num, scale)
