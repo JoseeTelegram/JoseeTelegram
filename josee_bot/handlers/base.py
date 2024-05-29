39550d3a4f0eaa1b2ffa3e542499c737077f60ca
LamberKeep <LamberKeep@gmail.com>
2024-05-29 21:17:40 +0300

    Add humanize package

--- josee_bot/handlers/base.py
+++ josee_bot/handlers/base.py
@@ -6,6 +6,7 @@ from time import ctime
 from time import time
 
 import emoji
+import humanize
 import psutil
 import requests
 from PIL import Image as IMG
@@ -346,34 +347,16 @@ start_time = time()
 
 @dp.message(Command("sysfetch"))
 async def cmd_sysfetch(msg: Message) -> None:
-    up_time = getReadableTime(round(time() - start_time))
-
     await msg.reply(
         f"<b>{' System Information ':─^30}</b>"
         f"\n<b>Platform:</b> {platform.system()} {platform.release()}"
         f"\n<b>Architecture:</b> {platform.machine()}"
-        f"\n<b>Uptime:</b> {up_time[0]}d {up_time[1]}h {up_time[2]}m {up_time[3]}s"
+        f"\n<b>Uptime:</b> {humanize.naturaldelta(time() - start_time)}"
         f"\n<b>CPU:</b> {round(psutil.cpu_freq()[0])}/{round(psutil.cpu_freq()[2])} GHz ({round(psutil.cpu_percent())}%)"
         f"\n<b>RAM:</b> {round(psutil.virtual_memory().used / 1024 ** 2)}/{round(psutil.virtual_memory().total / 1024 ** 2)} MB ({round(psutil.virtual_memory().percent)}%)"
         f"\n<b>IP:</b> {requests.get('http://icanhazip.com').text.rstrip()}"
         f"\n<b>{' System Information ':─^30}</b>"
         , 'HTML')
-
-    return
-
-
-def getReadableTime(seconds: int):
-    if seconds <= 0:
-        return False
-
-    days = seconds // 86400
-    seconds -= 86400 * days
-    hours = seconds // 3600
-    seconds -= 3600 * hours
-    minutes = seconds // 60
-    seconds -= 60 * minutes
-
-    return days, hours, minutes, seconds
 
 
 translator = Translator()
